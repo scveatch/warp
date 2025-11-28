b72410e90c1076740aa1c86c2a96ccc8d76b055e
scveatch <scveatch@willamette.edu>
2025-11-27 22:35:08 -0800

    ops: Update install.sh
    Set to dynamically gather latest release tarbell, ensure executable

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,14 @@ set -euo pipefail
 
 # Warp Installer Script
 # 
-# Downloads the latest warp.sh and warp-bin release assets from 
+# Downloads the latest warp.sh and warp release assets from 
 # Github and installs them to ~/.local/bin
 
 # Config
 INSTALL_DIR="${HOME}/.local/bin"
-REPO="scveatch/warp"
-FILES=("warp.sh" "warp-bin")
+REPO=$(curl -s "https://api.github.com/repos/scveatch/warp/releases/latest" | \
+    grep "browser_download_url.*tar.gz" | \
+    cut -d '"' -f 4)
 
 # Check for curl existence:
 if ! command -v curl >/dev/null 2>&1; then
@@ -23,13 +24,14 @@ mkdir -p "${INSTALL_DIR}"
 echo "Installing Warp to ${INSTALL_DIR}"
 
 # Download files
-for f in "${FILES[@]}"; do 
-    URL="https://github.com/${REPO}/releases/latest/download/${f}"
-    TARGET="${INSTALL_DIR}/${f}"
-    echo "Downloading ${f}..."
-    curl -sL "${URL}" -o "${TARGET}"
-    chmod +x "${TARGET}"
-done 
+curl -sSL -o /tmp/warp.tar.gz "${REPO}"
+echo "Downloaded tar to /tmp/warp.tar.gz"
+
+# Unpack Tar 
+tar -tzf /tmp/warp.tar.gz -C "${INSTALL_DIR}"
+
+# Ensure Executable
+chmod +x "${INSTALL_DIR}/warp" "${INSTALL_DIR}/warp.sh"
 
 # Add install dir to PATH for this session if not already present
 if [[ ":$PATH:" != *":$INSTALL_DIR:"* ]]; then
